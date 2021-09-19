98a25ce45668f193779ad717711b0401dae7deda
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-09-19 11:31:28 +0600

    project structure setup

---
+++ src/components/body/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Body() {
+    return (
+        <div>
+            this is body
+        </div>
+    )
+}
+
+export default Body
---
+++ src/components/home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from "../header/Header";
+import Body from "../body/Body";
+import Footer from "../footer/Footer";
+import './home.css'
+function Home() {
+    return (
+        <div className="home">
+        <Header/>
+        <Body/>
+        <Footer/>
+           
+        </div>
+    )
+}
+
+export default Home
